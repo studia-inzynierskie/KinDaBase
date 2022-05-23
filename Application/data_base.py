3f25367ce98981cf82a06120bf7d537f34e14577
bartek <rodzikbartek@gmail.com>
2022-05-23 21:23:42 +0200

    Add some functions

--- Application/data_base.py
+++ Application/data_base.py
@@ -5,7 +5,7 @@ class FilmWeb():
         self.connection = cx_Oracle.connect(f'{user}/{password}@//ora4.ii.pw.edu.pl:1521/pdb1.ii.pw.edu.pl')
         self.option = 1
 
-        self.menu_list = ["what you gonna do ?",
+        self.menu_list = ["\nWhat you gonna do?",
                           "1 - add movie review",
                           "2 - show all movies",
                           "3 - show movie rating",
@@ -15,34 +15,61 @@ class FilmWeb():
                           "7 - show where actor played",
                           "8 - show best rated actor",
                           "9 - show movies and series with category",
-                          "0 - nothing. Bye"]
+                          "0 - nothing. Bye\n\n"]
 
     def __del__(self):
-        self.connection.close()
+        self.connection.close(self)
 
     def menu(self):
         while self.option:
             self.print_menu()
             self.option = int(input())
             if self.option == 1:
-                pass
+                self.add_movie()
             elif self.option == 2:
-                pass
+                self.show_all_movies()
             elif self.option == 3:
-                pass
+                self.show_movie_rating()
             elif self.option == 4:
-                pass
+                self.show_series_information()
             elif self.option == 5:
-                pass
+                self.show_actors_in_series()
             elif self.option == 6:
-                pass
+                self.add_actor()
             elif self.option == 7:
-                pass
+                self.show_where_actor_played()
             elif self.option == 8:
-                pass
+                self.show_best_rated_actor()
             elif self.option == 9:
-                pass
+                self.show_movies_series_with_category()
 
+
+    def add_movie(self):
+        print("Film added")
+
+    def show_all_movies(self):
+        pass
+
+    def show_movie_rating(self):
+        pass
+
+    def show_series_information(self):
+        pass
+    
+    def show_actors_in_series(self):
+        pass
+    
+    def add_actor(self):
+        pass
+    
+    def show_where_actor_played(self):
+        pass
+    
+    def show_best_rated_actor(self):
+        pass
+    
+    def show_movies_series_with_category(self):
+        pass
 
     def print_menu(self):
         for op in self.menu_list:
